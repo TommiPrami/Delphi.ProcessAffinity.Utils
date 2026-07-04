da4a2d1aec71bade27d15af9d7e9aec488cc48af
TommiPrami <tommi.prami@gmail.com>
2026-07-04 08:22:20 +0300

    Bug fixes and retructuring of the Repository, and skeleton for a demo

---
+++ Demo/DPAUForm.Main.pas
@@ -0,0 +1,27 @@
+﻿unit DPAUForm.Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TDPAUMainForm = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  DPAUMainForm: TDPAUMainForm;
+
+implementation
+
+uses
+  Delphi.ProcessAffinity.Utils;
+
+{$R *.dfm}
+
+end.
